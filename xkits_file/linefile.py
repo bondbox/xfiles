8a8a07073c336d8d81d21495c352c3ee3ac44d0d
Mingzhe Zou <zoumingzhe@qq.com>
2025-04-24 20:37:00 +0800

    linefile: add LineFile class for handling line-based files

---
+++ xkits_file/linefile.py
@@ -0,0 +1,231 @@
+# coding:utf-8
+
+from ctypes import Structure
+from ctypes import addressof  # noqa:H306
+from ctypes import c_uint8
+from ctypes import c_uint32  # noqa:H306
+from ctypes import memmove
+from typing import BinaryIO
+
+from xkits_file.safefile import BaseFile
+
+
+class LineFile(BaseFile):
+
+    class Metadata(Structure):
+        """The metadata of a line is stored at the front and back of the data
+
+        Attributes:
+            magic(4bytes): magic number (0x5c, 0x3a, 0x6c, 0x66)
+            order(uint32): serial number, start from 1
+            bytes(uint32): the length of data in the line
+        """
+        SINGLE: int = 12
+        DOUBLE: int = SINGLE * 2
+        MAGIC = b"\x5c\x3a\x6c\x66"
+
+        _fields_ = [
+            ("magic", c_uint8 * 4),
+            ("order", c_uint32),
+            ("bytes", c_uint32),
+        ]
+
+        def __str__(self):
+            return f"{__class__.__name__}(order={self.order}, bytes={self.bytes})"  # noqa:E501
+
+        def __eq__(self, other) -> bool:
+            return self.order == other.order and self.bytes == other.bytes\
+                if isinstance(other, self.__class__) else False
+
+        def check_magic(self) -> bool:
+            return bytes(self.magic) == self.MAGIC
+
+        @classmethod
+        def check_order(cls, order: int) -> bool:
+            return 0 < order <= 0x7fffffff
+
+        @classmethod
+        def check_bytes(cls, bytes: int) -> bool:  # pylint: disable=W0622
+            return 0 < bytes <= 0x7fffffff
+
+        @classmethod
+        def parse(cls, datas: bytes):
+            if len(datas) < cls.SINGLE:
+                raise ValueError(f"Invalid datas: {datas}")
+
+            instance = cls()
+            memmove(addressof(instance), datas, cls.SINGLE)
+            if not instance.check_magic():
+                raise ValueError(f"Invalid magic: {instance.magic}")
+            if not instance.check_order(instance.order):
+                raise ValueError(f"Invalid order: {instance.order}")
+            if not instance.check_bytes(instance.bytes):
+                raise ValueError(f"Invalid bytes: {instance.bytes}")
+            return instance
+
+        @classmethod
+        def new(cls, order: int, bytes: int):  # pylint: disable=W0622
+            if not cls.check_order(order):
+                raise ValueError(f"Invalid serial: {order}")
+
+            if not cls.check_bytes(bytes):
+                raise ValueError(f"Invalid length: {bytes}")
+
+            instance = cls()
+            instance.magic = (c_uint8 * 4)(*cls.MAGIC)  # pylint: disable=W0201
+            instance.order = order  # pylint: disable=W0201
+            instance.bytes = bytes  # pylint: disable=W0201
+            return instance
+
+    class Cursor():
+        """The cursor of a line
+
+        Attributes:
+            serial(int): serial number, start from 1
+            offset(int): start position of the line
+            length(int): the length of data in the line
+        """
+
+        def __init__(self, serial: int, offset: int, content: bytes):
+            length: int = len(content)
+            if not (serial == 0 and offset == 0 and content == b""):
+                self.check(serial, offset, length)
+            self.__serial: int = serial
+            self.__offset: int = offset
+            self.__length: int = length
+            self.__content: bytes = content
+
+        def __str__(self):
+            return f"{__class__.__name__}(serial={self.serial}, offset={self.offset}, length={self.length})"  # noqa:E501
+
+        def __bool__(self):
+            return self.serial > 0
+
+        @property
+        def serial(self) -> int:
+            return self.__serial
+
+        @property
+        def offset(self) -> int:
+            return self.__offset
+
+        @property
+        def length(self) -> int:
+            return self.__length
+
+        @property
+        def content(self) -> bytes:
+            return self.__content
+
+        @property
+        def prev_tail_offset(self) -> int:
+            """the start position of prev metadata"""
+            if self.serial <= 1:
+                raise StopIteration("This is already the first line")
+
+            return self.offset - LineFile.Metadata.SINGLE
+
+        @property
+        def next_head_offset(self) -> int:
+            "the start position of next and the end position of current"
+            return self.offset + self.length + LineFile.Metadata.DOUBLE if self.serial > 0 else 0  # noqa:E501
+
+        def prev(self, content: bytes):
+            if (serial := self.serial - 1) < 1:
+                raise StopIteration("This is already the first line")
+
+            offset: int = self.offset - len(content) - LineFile.Metadata.DOUBLE  # noqa:E501
+            return LineFile.Cursor(serial, offset, content)
+
+        def next(self, content: bytes):
+            return LineFile.Cursor(self.serial + 1, self.next_head_offset, content)  # noqa:E501
+
+        @classmethod
+        def check(cls, serial: int, offset: int, length: int):
+            if not LineFile.Metadata.check_order(serial):
+                raise ValueError(f"Invalid serial: {serial}")
+
+            if not LineFile.Metadata.check_bytes(length):
+                raise ValueError(f"Invalid length: {length}")
+
+            if offset < (LineFile.Metadata.DOUBLE + 1) * (serial - 1) or (serial == 1 and offset != 0):  # noqa:E501
+                raise ValueError(f"Invalid offset: {offset} (serial {serial})")
+
+        @classmethod
+        def begin(cls):
+            return cls(0, 0, b"")
+
+    def __init__(self, filepath: str, readonly: bool = True) -> None:
+        self.__cursor: LineFile.Cursor = LineFile.Cursor.begin()
+        super().__init__(filepath=filepath, readonly=readonly)
+        assert super().open() is self.binary, "open failed"
+        self.__cursor = self.check()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super().close()
+
+    def __check_line(self, fd: BinaryIO, sn: int) -> Cursor:
+        offset: int = fd.tell()
+
+        head = self.Metadata.parse(fd.read(LineFile.Metadata.SINGLE))
+        data: bytes = fd.read(head.bytes)
+        tail = self.Metadata.parse(fd.read(LineFile.Metadata.SINGLE))
+        if head != tail or head.order != sn:
+            raise ValueError(f"sn: {sn}, {head} != {tail}")
+
+        assert (endpos := fd.tell()) == offset + len(data) + LineFile.Metadata.DOUBLE, f"endpos({endpos}) error"  # noqa:E501
+        return self.Cursor(sn, offset, data)
+
+    def fast_check(self) -> Cursor:
+        fhdl: BinaryIO = self.binary
+
+        try:
+            if (endpos := fhdl.tell()) <= LineFile.Metadata.DOUBLE:
+                raise Warning(f"{endpos} <= {LineFile.Metadata.DOUBLE}")
+
+            if fhdl.seek(-LineFile.Metadata.SINGLE, 2) != endpos - LineFile.Metadata.SINGLE:  # noqa:E501
+                raise BufferError("seek overflow")  # pragma: no cover
+
+            tail = self.Metadata.parse(fhdl.read(LineFile.Metadata.SINGLE))
+            if endpos < (length := tail.bytes + LineFile.Metadata.DOUBLE) or fhdl.seek(-length, 1) != endpos - length:  # noqa:E501
+                raise Warning(f"{endpos} < {length + LineFile.Metadata.DOUBLE}")  # noqa:E501, pragma: no cover
+
+            if (cursor := self.__check_line(fhdl, tail.order)).next_head_offset != endpos:  # noqa:E501
+                raise Warning("unexpected end of file")  # pragma: no cover
+            return cursor
+        except (ValueError, BufferError, Warning):
+            return self.Cursor.begin()
+
+    def full_check(self) -> Cursor:
+        cursor: LineFile.Cursor = self.Cursor.begin()
+        fhdl: BinaryIO = self.binary
+        fhdl.seek(0, 0)
+
+        try:
+            while True:
+                cursor = self.__check_line(fhdl, cursor.serial + 1)
+        except (ValueError, BufferError):
+            assert fhdl.seek(endpos := cursor.next_head_offset, 0) == endpos, "seek failed"  # noqa:E501
+            if not self.readonly:
+                assert fhdl.truncate(endpos) == endpos, "truncate failed"
+
+        return cursor
+
+    def check(self) -> Cursor:
+        return self.fast_check() or self.full_check()
+
+    def dump(self, datas: bytes) -> Cursor:
+        if not self.readonly and len(datas) > 0:
+            fhdl: BinaryIO = self.binary
+            next = self.__cursor.next(datas)  # pylint: disable=W0622
+            if fhdl.tell() != next.offset or fhdl.seek(next.offset, 0) != next.offset:  # noqa:E501
+                raise Warning(f"file position {fhdl.tell()} != {next.offset}")  # noqa:E501, pragma: no cover
+            meta = self.Metadata.new(order=next.serial, bytes=next.length)
+            assert fhdl.write(bytes(meta)) == LineFile.Metadata.SINGLE
+            assert fhdl.write(next.content) == next.length, "write data error"
+            assert fhdl.write(bytes(meta)) == LineFile.Metadata.SINGLE
+            self.__cursor = next
+        return self.__cursor
---
+++ xkits_file/unittest/test_linefile.py
@@ -0,0 +1,197 @@
+# coding:utf-8
+
+from os.path import join
+from tempfile import TemporaryDirectory
+import unittest
+
+from xkits_file.linefile import LineFile
+
+
+class TestLineFile(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_metadata_parse(self):
+        self.assertIsInstance(LineFile.Metadata.parse(b"\x5c\x3a\x6c\x66\x01\x00\x00\x00\xff\xff\xff\x7f"), LineFile.Metadata)  # noqa:E501
+        self.assertIsInstance(LineFile.Metadata.parse(b"\x5c\x3a\x6c\x66\xff\xff\xff\x7f\x01\x00\x00\x00"), LineFile.Metadata)  # noqa:E501
+        self.assertRaises(ValueError, LineFile.Metadata.parse, b"\x5c\x3a\x6c\x66\xff\xff\xff\xff\xff\xff\xff\x7f")  # noqa:E501
+        self.assertRaises(ValueError, LineFile.Metadata.parse, b"\x5c\x3a\x6c\x66\x00\x00\x00\x00\xff\xff\xff\x7f")  # noqa:E501
+        self.assertRaises(ValueError, LineFile.Metadata.parse, b"\x5c\x3a\x6c\x66\xff\xff\xff\x7f\xff\xff\xff\xff")  # noqa:E501
+        self.assertRaises(ValueError, LineFile.Metadata.parse, b"\x5c\x3a\x6c\x66\xff\xff\xff\x7f\x00\x00\x00\x00")  # noqa:E501
+
+    def test_metadata_new(self):
+        self.assertIsInstance((meta := LineFile.Metadata.new(1, 1)), LineFile.Metadata)  # noqa:E501
+        self.assertRaises(ValueError, LineFile.Metadata.new, 1, 0)
+        self.assertRaises(ValueError, LineFile.Metadata.new, 0, 1)
+        self.assertEqual(str(meta), "Metadata(order=1, bytes=1)")
+
+    def test_cursor(self):
+        self.assertIsInstance((cursor := LineFile.Cursor(1, 0, 1)), LineFile.Cursor)  # noqa:E501
+        self.assertRaises(ValueError, LineFile.Cursor, 1, -1, 1)
+        self.assertRaises(ValueError, LineFile.Cursor, 1, 1, 1)
+        self.assertRaises(ValueError, LineFile.Cursor, 1, 0, 0)
+        self.assertRaises(ValueError, LineFile.Cursor, 0, 0, 1)
+        self.assertEqual(str(cursor), "Cursor(serial=1, offset=0, length=1)")
+        self.assertEqual(cursor.serial, 1)
+        self.assertEqual(cursor.offset, 0)
+        self.assertEqual(cursor.length, 1)
+
+    def test_cursor_prev(self):
+        self.assertIsInstance((cursor2 := LineFile.Cursor(2, 25, 1)), LineFile.Cursor)  # noqa:E501
+        self.assertIsInstance((cursor1 := cursor2.prev(1)), LineFile.Cursor)
+        self.assertRaises(StopIteration, cursor1.prev, 1)
+        self.assertRaises(ValueError, cursor2.prev, 2)
+        self.assertEqual(cursor2.prev_tail_offset, 13)
+        self.assertEqual(cursor1.serial, 1)
+        self.assertEqual(cursor1.offset, 0)
+        self.assertEqual(cursor1.length, 1)
+
+    def test_cursor_begin(self):
+        def prev(cursor: LineFile.Cursor) -> int:
+            return cursor.prev_tail_offset
+
+        self.assertIsInstance((cursor := LineFile.Cursor.begin()), LineFile.Cursor)  # noqa:E501
+        self.assertEqual(str(cursor), "Cursor(serial=0, offset=0, length=0)")
+        self.assertIsInstance((next := cursor.next(2)), LineFile.Cursor)
+        self.assertRaises(StopIteration, cursor.prev, 3)
+        self.assertRaises(StopIteration, prev, cursor)
+        self.assertEqual(cursor.serial, 0)
+        self.assertEqual(cursor.offset, 0)
+        self.assertEqual(cursor.length, 0)
+        self.assertEqual(next.serial, 1)
+        self.assertEqual(next.offset, 0)
+        self.assertEqual(next.length, 2)
+        self.assertFalse(cursor)
+        self.assertTrue(next)
+
+    def test_create(self):
+        with TemporaryDirectory() as temp:
+            self.assertRaises(FileNotFoundError, LineFile, join(temp, "test"))
+
+    def test_dump(self):
+        with TemporaryDirectory() as temp:
+            with LineFile(join(temp, "test"), readonly=False) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor0 := line.fast_check(), LineFile.Cursor)  # noqa:E501
+                self.assertIsInstance(cursor1 := line.dump(b"demo"), LineFile.Cursor)  # noqa:E501
+                self.assertEqual(cursor0.serial, 0)
+                self.assertEqual(cursor0.offset, 0)
+                self.assertEqual(cursor0.length, 0)
+                self.assertEqual(cursor1.serial, 1)
+                self.assertEqual(cursor1.offset, 0)
+                self.assertEqual(cursor1.length, 4)
+
+            with LineFile(join(temp, "test"), readonly=True) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor := line.check(), LineFile.Cursor)
+                self.assertEqual(cursor.serial, 1)
+                self.assertEqual(cursor.offset, 0)
+                self.assertEqual(cursor.length, 4)
+
+    def test_dump_error_serial(self):
+        with TemporaryDirectory() as temp:
+            with LineFile(join(temp, "test"), readonly=False) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor := line.dump(b"demo1"), LineFile.Cursor)  # noqa:E501
+                meta = LineFile.Metadata.new(order=(next := cursor.next(5)).serial, bytes=5)  # noqa:E501
+                line.binary.write(bytes(meta))
+                line.binary.write(b"demo2")
+                meta.order = 123
+                line.binary.write(bytes(meta))
+                self.assertEqual(cursor.serial, 1)
+                self.assertEqual(cursor.offset, 0)
+                self.assertEqual(cursor.length, 5)
+                self.assertEqual(next.serial, 2)
+                self.assertEqual(next.offset, 29)
+                self.assertEqual(next.length, 5)
+                self.assertEqual(line.binary.tell(), 58)
+
+            with LineFile(join(temp, "test"), readonly=True) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor := line.check(), LineFile.Cursor)
+                self.assertEqual(cursor.serial, 1)
+                self.assertEqual(cursor.offset, 0)
+                self.assertEqual(cursor.length, 5)
+                self.assertEqual(line.binary.tell(), 29)
+                self.assertEqual(line.binary.seek(0, 2), 58)
+                self.assertEqual(line.binary.tell(), 58)
+
+            with LineFile(join(temp, "test"), readonly=False) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor := line.check(), LineFile.Cursor)
+                self.assertEqual(cursor.serial, 1)
+                self.assertEqual(cursor.offset, 0)
+                self.assertEqual(cursor.length, 5)
+                self.assertEqual(line.binary.tell(), 29)
+                self.assertEqual(line.binary.seek(0, 2), 29)
+                self.assertEqual(line.binary.tell(), 29)
+
+    def test_check_dirty(self):
+        with TemporaryDirectory() as temp:
+            with LineFile(join(temp, "test"), readonly=False) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertEqual(line.binary.write(b"1234567890"), 10)
+                self.assertEqual(line.binary.tell(), 10)
+
+            with LineFile(join(temp, "test"), readonly=True) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor := line.check(), LineFile.Cursor)
+                self.assertEqual(cursor.serial, 0)
+                self.assertEqual(cursor.offset, 0)
+                self.assertEqual(cursor.length, 0)
+
+    def test_check_and_truncate(self):
+        with TemporaryDirectory() as temp:
+            with LineFile(join(temp, "test"), readonly=False) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor1 := line.dump(b"demo1"), LineFile.Cursor)  # noqa:E501
+                self.assertIsInstance(cursor2 := line.dump(b"demo2"), LineFile.Cursor)  # noqa:E501
+                self.assertIsInstance(cursor3 := line.dump(b"demo3"), LineFile.Cursor)  # noqa:E501
+                self.assertEqual(cursor1.serial, 1)
+                self.assertEqual(cursor1.offset, 0)
+                self.assertEqual(cursor1.length, 5)
+                self.assertEqual(cursor2.serial, 2)
+                self.assertEqual(cursor2.offset, 29)
+                self.assertEqual(cursor2.length, 5)
+                self.assertEqual(cursor3.serial, 3)
+                self.assertEqual(cursor3.offset, 58)
+                self.assertEqual(cursor3.length, 5)
+                self.assertEqual(line.binary.tell(), 87)
+                self.assertEqual(line.binary.seek(80, 0), 80)
+                self.assertEqual(line.binary.truncate(), 80)
+
+            with LineFile(join(temp, "test"), readonly=True) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor := line.check(), LineFile.Cursor)
+                self.assertEqual(cursor.serial, 2)
+                self.assertEqual(cursor.offset, 29)
+                self.assertEqual(cursor.length, 5)
+                self.assertEqual(line.binary.tell(), 58)
+                self.assertEqual(line.binary.seek(0, 2), 80)
+                self.assertEqual(line.binary.tell(), 80)
+
+            with LineFile(join(temp, "test"), readonly=False) as line:
+                self.assertIsInstance(line, LineFile)
+                self.assertIsInstance(cursor := line.check(), LineFile.Cursor)
+                self.assertEqual(cursor.serial, 2)
+                self.assertEqual(cursor.offset, 29)
+                self.assertEqual(cursor.length, 5)
+                self.assertEqual(line.binary.tell(), 58)
+                self.assertEqual(line.binary.seek(0, 2), 58)
+                self.assertEqual(line.binary.tell(), 58)
+
+
+if __name__ == "__main__":
+    unittest.main()
