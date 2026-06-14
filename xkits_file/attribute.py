7747b0f3c49f94a5a32207af60fa0d1274f862a3
Mingzhe Zou <zoumingzhe@qq.com>
2026-06-15 02:23:35 +0800

    build: remove unused xpip-build dependency from pyproject.toml

--- xkits_file/attribute.py
+++ xkits_file/attribute.py
@@ -1,8 +1,11 @@
 # coding:utf-8
 
-__version__ = "0.10"
-
 # project info
 __project_name__ = "xfiles"
 __project_home__ = "https://github.com/bondbox/xfiles/"
 __project_desc__ = "File module"
+
+# package info
+__version__ = "0.10"
+__package_name__ = "xkits-file"
+__package_desc__ = "File module"
